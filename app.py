286f452a9100f5d9dfbe5bb4f1f81a2c2d24d900
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2025-09-05 14:43:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Finance Analyzer is running!"
+    return "Finance Analyzer is running!"
 
 if __name__ == "__main__":
     app.run(debug=True)
